a1840e379cee40e42c793beeee037cf9e4feda22
PatrykZajac <patryk.zajac0@gmail.com>
2019-11-21 21:48:11 +0100

    Changes: Add delete class

---
+++ src/main/java/com/lambda/Delete.java
@@ -0,0 +1,19 @@
+package com.lambda;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsyncClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.lambda.runtime.Context;
+
+public class Delete {
+    public static Object handleRequest(Test request, Context context) {
+        if (Authorizer.verify(request.getUser().getUserToken())) {
+            AmazonDynamoDB client = AmazonDynamoDBAsyncClientBuilder.defaultClient();
+            DynamoDBMapper mapper = new DynamoDBMapper(client);
+            mapper.delete(request);
+            return true;
+        } else
+            return false;
+    }
+
+}
